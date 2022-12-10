0092838382149bdb71f9109bb1ea7a1c1063233f
p2arthur <arthurrabelopro@gmail.com>
2022-12-10 11:21:01 -0300

    Started the function to create a book on the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import BookCreate from "./components/BookCreate";
 import BookList from "./components/BookList";
+import { useState } from "react";
 
 function App() {
+  const [booksList, setBooksList] = useState([]);
+
+  const createBook = (title) => {
+    setBooksList([...booksList, { title: title }]);
+    console.log(booksList);
+  };
+
   return (
     <div>
       <div>Hello world</div>
+      {booksList.length}
       <BookList />
-      <BookCreate />
+      <BookCreate onCreate={createBook} />
     </div>
   );
 }
--- src/components/BookCreate.jsx
+++ src/components/BookCreate.jsx
@@ -1,5 +1,35 @@
-function BookCreate() {
-  return <div>BookCreate</div>;
+import { useState } from "react";
+
+function BookCreate({ onCreate }) {
+  const [title, setTitle] = useState("");
+
+  //Controlled component for the input
+  const handleInputChange = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onCreate(title);
+    setTitle("");
+  };
+
+  return (
+    <div className="book-create">
+      <h3>Add a book</h3>
+      <form onSubmit={handleSubmit} action="">
+        <label htmlFor="create-book__input">Book title:</label>
+        <input
+          className="input"
+          id="create-book__input"
+          value={title}
+          onChange={handleInputChange}
+          type="text"
+        />
+        <button className="button">Submit</button>
+      </form>
+    </div>
+  );
 }
 
 export default BookCreate;
